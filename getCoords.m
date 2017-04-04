9316224863f0cc1b7270c51a3be4d644bb0ba94a
Raúl Hernández <raul@lafuentelab.org>
2017-04-04 14:50:17 -0500

    a

---
+++ getCoords.m
@@ -0,0 +1,26 @@
+function getCoords(fileName,varargin)
+
+    mask = getArgumentValue('mask',false,varargin{:},'warningoff');
+    operation = getArgumentValue('operation','max',varargin{:},'warningoff');
+
+    data = load_untouch_nii(fileName);
+    if mask
+        maskData = load_untouch_nii(mask);
+        data.img(:) = data.img(:).* (maskData.img(:) ~= 0); 
+    end
+
+
+
+[val,coords] = Max3d(data.img);
+data.img(coords(1),coords(2),coords(3)) = 0;   
+['Coordinates: ',num2str(coords), ' value: ', num2str(val)]
+[val2,coords2] = Max3d(data.img);
+
+while val == val2
+    [val,coords] = Max3d(data.img);
+    ['Coordinates: ',num2str(coords), ' value: ', num2str(val)]
+    data.img(coords(1),coords(2),coords(3)) = 0;   
+    [val2,coords2] = Max3d(data.img);
+    
+end
+
